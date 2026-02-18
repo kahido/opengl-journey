80492b63c1c194a2d5cce279561654deee68cd00
kahido <66074309+kahido@users.noreply.github.com>
2026-02-18 22:35:49 +0100

    Render Window with cleared color and catch ESC key

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,66 @@
 constexpr int const WIDTH = 640;
 constexpr int const HEIGHT = 480;
 
+namespace {
+
+void framebuffer_size_callback(GLFWwindow* /*window*/, int width, int height)
+{
+    spdlog::info("new framebuffer size [{0} x {1}]", width, height);
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        spdlog::info("GLFW_KEY_ESCAPE pressed");
+        glfwSetWindowShouldClose(window, 1);
+    }
+}
+}
+
 int main()
 {
     if (glfwInit() == 0)
     {
-        spdlog::error("GLFW Init Error!");
-        return 1;
+        spdlog::error("GLFW Init failed!");
+        return -1;
     }
 
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "[glad] GL with GLFW", nullptr, nullptr);
     if (window == nullptr)
     {
-        spdlog::error("Window Error!");
-        return 1;
+        spdlog::error("Failed create window!");
+        return -1;
     }
 
     glfwMakeContextCurrent(window);
+
     int const version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0)
+    {
+        spdlog::error("Failed load GL!");
+        return -1;
+    }
 
-    spdlog::info("GL {0}.{1}", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+    spdlog::info("Loaded GL {0}.{1}", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // render loop
+    while (glfwWindowShouldClose(window) == 0)
+    {
+        processInput(window);
+
+        glClearColor(0.2F, 0.3F, 0.3F, 1.0F);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
